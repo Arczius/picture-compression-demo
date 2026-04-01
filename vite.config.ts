7c3dabecd5fc10d31f36b8f7f0ec6658c424219e
Arczius <damianvaartmans@gmail.com>
2026-04-01 18:56:54 +0200

    changed eslint version

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import eslint from 'vite-plugin-eslint'
+import eslint from 'vite-plugin-eslint2'
 
 // https://vite.dev/config/
 export default defineConfig({
